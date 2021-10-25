be0cdd97ce6f5059583f08beafff78c73d192eeb
Gustav12dk00 <laugus11@hotmail.com>
2021-10-25 15:28:32 +0200

    Basic setup

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module server
+
+go 1.17
+
+replace chatpb => ../chatpb
--- server/server.go
+++ server/server.go
@@ -1,2 +1,21 @@
 package main
 
+import (
+	pb "chatpb"
+)
+
+func main() {
+	
+}
+
+func (s *pb.Server) CreateStream(pconn *pb.Connect, stream proto.Broadcast_CreateStreamServer) error {
+	conn := &Connection{
+	   stream: stream,
+	   id: pconn.User.Id,
+	   active: true,
+	   err : make(chan error),
+	}
+	s.Connection = append(s.Connection, conn)
+ 
+	return <-conn.err
+ }
